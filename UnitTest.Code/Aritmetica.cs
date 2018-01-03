6b5636e4e71965adc1c5c4c9927841d93c99a486
Cristopher Jr. Vivieca Ramos <cristopherviviecara025@gmail.com>
2018-01-03 02:57:06 -0400

    Creación del proyecto

---
+++ UnitTest.Code/Aritmetica.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest.Code
+{
+    public class Aritmetica
+    {
+        public decimal Suma(int x, int y)
+        {
+            return x + y;
+        }
+
+        public decimal Resta(int x, int y)
+        {
+            return x - y;
+        }
+
+        public decimal Multiplicacion(int x, int y)
+        {
+            return x * y;
+        }
+
+        public decimal Division(int x, int y)
+        {
+            return x / y;
+        }
+    }
+}
---
+++ UnitTest.Test/AritmeticaTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTest.Code;
+
+namespace UnitTest.Test
+{
+    [TestClass]
+    public class AritmeticaTest
+    {
+        [TestMethod]
+        public void Suma_SumaDosNumeros_RetornaTrue()
+        {
+            //  Arrange
+            var aritmetica = new Aritmetica();
+
+            //  Act
+            var resultado = aritmetica.Suma(1,4);
+
+            //  Assert
+            Assert.IsTrue(resultado == 5);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void Division_DivisionEntreCero_RetornaDivideByZeroException()
+        {
+            //  Arrange
+            var aritmetica = new Aritmetica();
+
+            //  Act
+            aritmetica.Division(5, 0);
+
+            // Assert no necesario.
+        }
+    }
+}
